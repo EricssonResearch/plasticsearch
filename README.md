30b57f92a54bace8c8dda1b6a4614b3c4132bf5b
ehonlia <hongxin.liang@ericsson.com>
2015-04-13 14:04:00 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# plasticsearch
+
+
+## Installation
+
+Using SWI-Prolog 7 or later.
+
+    ?- pack_install('http://git.cf.ericsson.net/ehonlia/plasticsearch.git').
+
+Source code available and pull requests accepted
+[here](http://git.cf.ericsson.net/ehonlia/plasticsearch).
+
+@author Hongxin Liang <hongxin.liang@ericsson.com>
+
+@license TBD
+
+## Examples
---
+++ pack.pl
@@ -0,0 +1,9 @@
+name(plasticsearch).
+version('0.1').
+title('Elasticsearch client API').
+keywords([elasticsearch, client, API]).
+author('Hongxin Liang', 'hongxin.liang@ericsson.com').
+packager('Hongxin Liang', 'hongxin.liang@ericsson.com').
+maintainer('Hongxin Liang', 'hongxin.liang@ericsson.com').
+home('http://git.cf.ericsson.net/ehonlia/plasticsearch').
+download('http://git.cf.ericsson.net/ehonlia/plasticsearch/repository/archive.tar.gz').
