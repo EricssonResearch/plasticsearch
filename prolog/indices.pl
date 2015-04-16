c4d7e56ee2405ec6b6ffbd42c4a568d85134a9fb
Hongxin Liang <hxliang1982@gmail.com>
2015-04-16 23:47:43 +0200

    reorder according to python api

--- prolog/indices.pl
+++ prolog/indices.pl
@@ -1,20 +1,20 @@
 :- module(indices, [
-    create/4,       % +Ps, +Index, +Body, -Reply
-    create/5,       % +Ps, +Index, +Params, +Body, -Reply
-    delete/3,       % +Ps, +Index, -Reply
-    delete/4,       % +Ps, +Index, +Params, -Reply
-    get/4,          % +Ps, +Index, +Feature, -Reply
-    get/5,          % +Ps, +Index, +Feature, +Params, -Reply
     analyze/4,      % +Ps, +Index, +Body, -Reply
     analyze/5,      % +Ps, +Index, +Params, +Body, -Reply
     refresh/3,      % +Ps, +Index, -Reply
     refresh/4,      % +Ps, +Index, +Params, -Reply
     flush/3,        % +Ps, +Index, -Reply
     flush/4,        % +Ps, +Index, +Params, -Reply
+    create/4,       % +Ps, +Index, +Body, -Reply
+    create/5,       % +Ps, +Index, +Params, +Body, -Reply
+    get/4,          % +Ps, +Index, +Feature, -Reply
+    get/5,          % +Ps, +Index, +Feature, +Params, -Reply
     open_index/3,   % +Ps, +Index, -Reply
     open_index/4,   % +Ps, +Index, +Params, -Reply
     close_index/3,  % +Ps, +Index, -Reply
     close_index/4,  % +Ps, +Index, +Params, -Reply
+    delete/3,       % +Ps, +Index, -Reply
+    delete/4,       % +Ps, +Index, +Params, -Reply
     exists/2,       % +Ps, +Index
     exists/3,       % +Ps, +Index, +Params
     put_mapping/5,  % +Ps, +Index, +DocType, +Body, -Reply
@@ -34,48 +34,6 @@ index settings, aliases, mappings, index templates and warmers.
 
 :- use_module(transport).
 :- use_module(util).
-
-%% create(+Ps, +Index, +Body, -Reply) is semidet.
-%% create(+Ps, +Index, +Param, +Body, -Reply) is semidet.
-%
-% Create an index in Elasticsearch.
-% See [here](http://www.elastic.co/guide/en/elasticsearch/reference/current/indices-create-index.html).
-
-create(Ps, Index, Body, Reply) :-
-    create(Ps, Index, _{}, Body, Reply).
-
-create(Ps, Index, Params, Body, Reply) :-
-    non_empty(Index, index),
-    make_context(Index, Context),
-    perform_request(Ps, post, Context, Params, Body, _, Reply).
-
-%% delete(+Ps, +Index, -Reply) is semidet.
-%% delete(+Ps, +Index, +Params, -Reply) is semidet.
-%
-% Delete an index in Elasticsearch.
-% See [here](http://www.elastic.co/guide/en/elasticsearch/reference/current/indices-delete-index.html).
-
-delete(Ps, Index, Reply) :-
-    delete(Ps, Index, _{}, Reply).
-
-delete(Ps, Index, Params, Reply) :-
-    non_empty(Index, index),
-    make_context(Index, Context),
-    perform_request(Ps, delete, Context, Params, _, Reply).
-
-%% get(+Ps, +Index, +Feature, -Reply) is semidet.
-%% get(+Ps, +Index, +Feature, +Params, -Reply) is semidet.
-%
-% The get index API allows to retrieve information about one or more indexes.
-% See [here](http://www.elastic.co/guide/en/elasticsearch/reference/current/indices-get-index.html).
-
-get(Ps, Index, Feature, Reply) :-
-    get(Ps, Index, Feature, _{}, Reply).
-
-get(Ps, Index, Feature, Params, Reply) :-
-    non_empty(Index, index),
-    make_context([Index, Feature], Context),
-    perform_request(Ps, get, Context, Params, _, Reply).
 
 %% analyze(+Ps, +Index, +Body, -Reply) is semidet.
 %% analyze(+Ps, +Index, +Params, +Body, -Reply) is semidet.
@@ -120,6 +78,34 @@ flush(Ps, Index, Params, Reply) :-
     make_context([Index, '_flush'], Context),
     perform_request(Ps, post, Context, Params, '', _, Reply).
 
+%% create(+Ps, +Index, +Body, -Reply) is semidet.
+%% create(+Ps, +Index, +Param, +Body, -Reply) is semidet.
+%
+% Create an index in Elasticsearch.
+% See [here](http://www.elastic.co/guide/en/elasticsearch/reference/current/indices-create-index.html).
+
+create(Ps, Index, Body, Reply) :-
+    create(Ps, Index, _{}, Body, Reply).
+
+create(Ps, Index, Params, Body, Reply) :-
+    non_empty(Index, index),
+    make_context(Index, Context),
+    perform_request(Ps, post, Context, Params, Body, _, Reply).
+
+%% get(+Ps, +Index, +Feature, -Reply) is semidet.
+%% get(+Ps, +Index, +Feature, +Params, -Reply) is semidet.
+%
+% The get index API allows to retrieve information about one or more indexes.
+% See [here](http://www.elastic.co/guide/en/elasticsearch/reference/current/indices-get-index.html).
+
+get(Ps, Index, Feature, Reply) :-
+    get(Ps, Index, Feature, _{}, Reply).
+
+get(Ps, Index, Feature, Params, Reply) :-
+    non_empty(Index, index),
+    make_context([Index, Feature], Context),
+    perform_request(Ps, get, Context, Params, _, Reply).
+
 %% open_index(+Ps, +Index, -Reply) is semidet.
 %% open_index(+Ps, +Index, +Params, -Reply) is semidet.
 %
@@ -148,6 +134,20 @@ close_index(Ps, Index, Params, Reply) :-
     non_empty(Index, index),
     make_context([Index, '_close'], Context),
     perform_request(Ps, post, Context, Params, '', _, Reply).
+
+%% delete(+Ps, +Index, -Reply) is semidet.
+%% delete(+Ps, +Index, +Params, -Reply) is semidet.
+%
+% Delete an index in Elasticsearch.
+% See [here](http://www.elastic.co/guide/en/elasticsearch/reference/current/indices-delete-index.html).
+
+delete(Ps, Index, Reply) :-
+    delete(Ps, Index, _{}, Reply).
+
+delete(Ps, Index, Params, Reply) :-
+    non_empty(Index, index),
+    make_context(Index, Context),
+    perform_request(Ps, delete, Context, Params, _, Reply).
 
 %% exists(+Ps, +Index) is semidet.
 %% exists(+Ps, +Index, +Params) is semidet.
